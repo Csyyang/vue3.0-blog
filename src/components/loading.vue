98d8a34343a4de7049aff8525c1b1e394131d9cf
csy23333 <693765678@qq.com>
2021-04-02 18:01:27 +0800

    feat: 初始化

---
+++ src/components/loading.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="loading">
+    <div class="box">
+      <img :src="require('@/assets/loading.png')">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@keyframes rotates {
+    to {
+        transform: rotate(0);
+    }
+    from {
+        transform: rotate(360deg);
+    }
+}
+#loading {
+  width: 100%;
+  height: 100%;
+  .box {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    margin: auto;
+    width: 64px;
+    height: 64px;
+    animation: rotates 4s linear;
+    animation-iteration-count: infinite;
+    animation-direction: reverse;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <loading @touchmove="test" v-show="isloading" />
+    <div v-show="!isloading">
+      233
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue'
+import loading from '@/components/loading'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    loading
+  },
+  setup() {
+    const isloading = ref(true)
+
+    // setTimeout(() => {
+    //   isloading.value = false
+    // },3000)
+
+
+
+    return {
+      isloading
+    }
+  }
+  ,methods: {
+    test(a) {
+      console.log(a)
+    }
   }
 }
 </script>
+
+
+<style lang="scss" scoped>
+.home {
+  height: 100%;
+  width: 100%;
+}
+</style>
