b594b84dd403e5686d58cf0eb61fe283ac648755
csy23333 <693765678@qq.com>
2021-06-28 11:26:52 +0800

    更新服务器

--- node/mysql/config.js
+++ node/mysql/config.js
@@ -1,9 +1,16 @@
+/*
+ * @Description: 页面描述
+ * @Author: 陈思羊
+ * @Date: 2021-06-25 10:21:26
+ * @LastEditors: 陈思羊
+ * @LastEditTime: 2021-06-25 11:09:34
+ */
 const configSql = {
   DATABASE: 'blog',
   USERNAME: 'root',
   PASSWORD: 'root',
   PORT: '3306',
-  HOST: '39.105.180.4'
+  HOST: '101.34.66.232'
 }
 
 module.exports = configSql;
